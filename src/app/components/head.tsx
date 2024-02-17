b914b298ad0dbd0af6715538214d5e0d0b27b3bf
Sandeep2325 <sandeepgowda2314@gmail.com>
2024-02-17 19:12:11 +0530

    first commit

---
+++ src/app/components/head.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function head() {
+  return (
+    <>
+    <title>Courses</title>
+    <meta content="width=device-width, initial-scale=1" name="viewport"/>
+    <meta name="description" content="Courses by sandeep"/>
+    <link rel="icon" href="../favicon.ico"/>
+    </>
+  )
+}
+
+export default head
